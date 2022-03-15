4c050812ee4bb05b1e514eed098a11f90a1c2bc2
AnishaSarkar13 <101517667+AnishaSarkar13@users.noreply.github.com>
2022-03-15 11:12:20 +0530

    2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace Hello
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = args[0];
+            
+            Console.WriteLine("Hello World{0}", name);
         }
     }
 }
