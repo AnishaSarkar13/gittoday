a2ed1dd23d0b14cc482902a709feaf856abd31ec
AnishaSarkar13 <101517667+AnishaSarkar13@users.noreply.github.com>
2022-03-15 10:23:14 +0530

    1
    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
